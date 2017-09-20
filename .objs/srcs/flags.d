4cd8f438556313b1f2985d6831ac22f4835916fd
Jeremy GUYET <jguyet@e1r9p3.42.fr>
2017-09-20 03:02:37 +0200

    kk

---
+++ .objs/srcs/flags.d
@@ -0,0 +1,3 @@
+.objs/srcs/flags.o: srcs/flags.c includes/ft_trace.h \
+  libft//includes/libft.h libft//includes/printf.h \
+  libft//includes/mapft.h includes/protocol_message.h
---
+++ .objs/srcs/network/socket_protocol.d
@@ -0,0 +1,3 @@
+.objs/srcs/network/socket_protocol.o: srcs/network/socket_protocol.c \
+  includes/ft_trace.h libft//includes/libft.h libft//includes/printf.h \
+  libft//includes/mapft.h includes/protocol_message.h
