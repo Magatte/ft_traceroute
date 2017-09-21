cafa51bc3f4a39d1455a767cab88f9a48e7481c9
Jeremy GUYET <jguyet@e1r9p3.42.fr>
2017-09-21 06:37:16 +0200

    add debug checksum

--- srcs/network/protocols/utils/sum.c
+++ srcs/network/protocols/utils/sum.c
@@ -31,7 +31,10 @@ unsigned short checksum(void *b, int len)
 		len -= 2;
 	}
 	if (len == 1)
+	{
+		printf("COUCOU == 1\n");
 		sum += buf[0] & 0xFF;
+	}
 	low = (sum & 0xFFFF);			/* prend les 2 octet de droite					*/
 	high = (sum >> 16);				/* prend les 2 octet de gauche					*/
 	sum = high + low;				/* aditionne le tout							*/
